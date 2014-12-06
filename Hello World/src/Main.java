5437acbb4f92211f3edf792fe92b25f4c93944a8
robotman3000 <robotman3000@icloud.com>
2014-12-06 14:36:08 -0500

    added say method with implementation

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -2,11 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main var = new Main();
+		
 		System.out.println("Hi, How are you");
 		System.out.println("Unnessesary Message");
 		
+		var.say("I am saying something");
 		
+	}
+	
+	public void say(String stringToSay){
+		System.out.println(stringToSay);
 	}
 
 }
